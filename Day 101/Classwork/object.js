47119496e56b13166ee6f9e30d116da43de800a3
Dataa123 <diasamidzed53@gmail.com>
2024-07-04 23:26:57 +0400

    commit

---
+++ Day 101/Classwork/object.js
@@ -0,0 +1,9 @@
+const person = {
+    info: ["CSS", "JavaScript", "Python"],
+    greet: function(){
+        console.log(person.name + " how are you?")
+    },
+    name: "Data"
+}
+
+export {person};
---
+++ Day 101/Classwork/script.js
@@ -0,0 +1,21 @@
+// 1. Function declarations and Arrow Functions
+// 2. Template Literals
+// 3. Short Conditionals: &&, ||, Ternary Operator
+// 4. Three Array Methods: .map(), .filter(), .reduce()
+// 5. Object Trics: Property, Shorthand, Destructuring, Spread Operator
+// 6. Promises + Async/Await Syntax
+// 7. ES Modules + Import / Export syntax
+
+// const ages = [13, 14, 18, 21, 45];
+
+// const newObj = ages.map((age) => age >= 18 ? {age, isAdult: "Yes"} : {age, isAdult: "No"});
+
+// console.log(newObj);
+
+import {person} from "./object.js"
+
+console.log(person);
+
+// when we know that we only have to export only one thing from another file, we can type:
+// export default X;
+// import Y from "./fileName.js"
